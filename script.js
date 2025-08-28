0988c508f7995293fc0036101cd113134ea8e469
MatheusVolponi <matheusponath20@gmail.com>
2025-08-28 11:09:45 -0300

    feat: Implementa a alteração do título conforme o contexto.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const longoButton = document.querySelector('.app__card-button--longo');
 const timeDisplay = document.querySelector('#timer');
 const banner = document.querySelector('.app__image');
 const title = document.querySelector('.app__title');
+
 const focoDuration = 1500;
 const curtoDuration = 300;
 const longoDuration = 900;
@@ -25,4 +26,20 @@ longoButton.addEventListener('click', () => {
 function changeContext (context) {
     html.setAttribute('data-context', context);
     banner.setAttribute('src', `/images/${context}.png`);
+    switch (context) {
+        case 'foco':
+            title.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case 'short':
+            title.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+            break;
+        case 'long':
+            title.innerHTML = `Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+            break;
+        default:
+            break;
+    }
 }
